57a960b2eb7080e022b9afe89b02a6093750abd8
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-12-06 11:25:53 -0500

    excavation et reparation

---
+++ services/fissure-beton/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<?php 
+    $language = "French";
+    $metaDesc ="Coffrage Phoenix est une entreprise se spécialisant en fondation de béton, en coffrage et en excavation dans Lanaudière, les Laurentides et la Rive-Nord.";
+    $englishUrl = "https://coffragesphoenix.com/";
+    include_once(__DIR__."/../../head.php");
+?>
+<body >
+  <?php include(__DIR__."/../../header.php")?>
+
+  <div class="container">
+    <div style="display: grid;" class="about">
+      <div style="text-align: center;">
+          <h2>Services <strong>coffrages</strong></h2>
+      </div>
+      <div style="text-align: center;margin-left: auto;margin-right: auto;"><img src="https://coffragesphoenix.com/assets/img/about-swig.svg" alt="about"/></div>
+  
+      <div class="desc">
+          <div class="desc-img">
+              <img src="https://coffragesphoenix.com/assets/img/reparation-de-fissure2.jpg" alt="fissure-beton" width="780" height="759">
+          </div>
+          <div class="servces-desc-title">
+              <h1 style="font-size: 53px !important;color:#545454;"><strong>Réparations de béton</strong></h1>
+              <hr>
+              <p style="color: #1e1e1e;">Chez Coffrage Phoenix nous offrons plusieurs types de réparation de béton.</p>
+              <ul class="browser-default">
+                  <li>Réparation d’une fondation</li>
+                  <li>Réparation de fissures de fondation</li>
+                  <li>Réparation marche de béton</li>
+                  <li>Réparation de balcon de béton</li>
+                  <li>Réparation des fissures sur dalle de béton</li>
+                  <li></li>
+              </ul>
+              <p style="color: #1e1e1e;">Avec notre équipe vous aller bénéficier de notre expertise. Nous nous engageons à vous offrir une solution adapter à vos exigences. N'hésitez pas à nous contacter pour tous vos besoins de réparation de béton pour donner une deuxième vie à vos structures de béton.</p>
+          </div>
+      </div>
+  
+      <div data-paperform-id="lesentrerpisesasraca"></div>
+    </div>
+  </div>
+  <?php include(__DIR__."/../../footing.php")?>
+</body>
+</html>
