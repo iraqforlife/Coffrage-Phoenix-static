484e18c883defbe61af2f2a3ebbcb15cee4beb9b
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-12-06 10:49:00 -0500

    balcon-en-beton

---
+++ services/balcon-en-beton/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<?php 
+    $language = "French";
+    $metaDesc ="Un balcon en béton est la solution idéale pour avoir un produit durable qui nécessite très peu d’entretien tout en résistant aux intempéries et aux années.";
+    $englishUrl = "https://coffragesphoenix.com/";
+    include_once(__DIR__."/../../head.php");
+?>
+<body >
+  <?php include(__DIR__."/../../header.php")?>
+
+  <div class="container">
+    <div style="display: grid;" class="about">
+      <div style="text-align: center;">
+          <h2>Services <strong>coffrages</strong></h2>
+      </div>
+      <div style="text-align: center;margin-left: auto;margin-right: auto;"><img src="https://coffragesphoenix.com/assets/img/about-swig.svg" alt="about"/></div>
+  
+      <div class="desc">
+          <div class="desc-img">
+              <img src="https://coffragesphoenix.com/assets/img/balcon-de-beton_004.jpg" alt="Coffrage-de-beton" width="780" height="759">
+          </div>
+          <div class="servces-desc-title">
+              <h1 style="color:#545454;"><strong>Balcons en béton</strong></h1>
+              <hr>
+              <p style="color: #1e1e1e;">Vous rêvez d'un espace extérieur qui est facile d'entretien, durable et esthétique? Notre balcon en béton est la structure idéale pour répondre à vos besoins. Les balcons en béton sont construits d'une façon durable, faite pour résister aux intempéries et aux fluctuations de température pour vous permettre de profiter de votre investissement durant plusieurs années. Le béton est un matériau de construction fiable et est maniable donc peut s'adapter à tout type de maison. Les galeries en béton nécessitent très peu d'entretien, ce qui veut dire adieux le vernissage du balcon , bye bye l'entretien du bois pour empêcher la moisissure. Les balcons en béton ont une durée de vie de plus de 50 ans contrairement aux balcons en bois qui ont une durée de vie de seulement 15-25 ans. Notre équipe d'experts assurera un travail impeccable et vous assura un produit final digne de votre vision.</p>
+          </div>
+      </div>
+  
+      <div data-paperform-id="lesentrerpisesasraca"></div>
+    </div>
+  </div>
+  <?php include(__DIR__."/../../footing.php")?>
+</body>
+</html>
