3099d84e6dddf6acdc63f7e92d35d724df3e819d
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-12-06 10:46:16 -0500

    agrandissement-maison

---
+++ services/agrandissement-maison/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<?php 
+    $language = "French";
+    $metaDesc ="Notre équipe d’experts assurera un produit de la meilleure qualité pour vous assurer un produit durable et résistant aux tremblements de terre pour l’agrandissement de votre maison.";
+    $englishUrl = "https://coffragesphoenix.com/";
+    include_once(__DIR__."/../../head.php");
+?>
+<body >
+  <?php include(__DIR__."/../../header.php")?>
+
+  <div class="container">
+    <div style="display: grid;" class="about">
+      <div style="text-align: center;">
+          <h2>Services <strong>coffrages</strong></h2>
+      </div>
+      <div style="text-align: center;margin-left: auto;margin-right: auto;"><img src="https://coffragesphoenix.com/assets/img/about-swig.svg" alt="about"/></div>
+  
+      <div class="desc">
+          <div class="desc-img">
+              <img src="https://coffragesphoenix.com/assets/img/fondation-agrandissement-de-maison_001.jpg" alt="Coffrage-de-beton" width="780" height="759">
+          </div>
+          <div class="servces-desc-title">
+              <h1 style="font-size: 42px !important;color:#545454;"><strong>Agrandissement de maison</strong></h1>
+              <hr>
+              <p style="color: #1e1e1e;">Pour tout projet d'agrandissement de maison, Coffrage Phoenix est là pour vous accompagner durant le processus. Nous allons travailler en collaboration avec vous pour comprendre tout vous besoins et attentes pour l'agrandissement de votre fondation. Notre équipe d'experts est présente pour exécuter votre vision et l'amener à la réalité. Pour l'agrandissement de votre fondation, notre équipe utilise des matériaux de qualités et utilise des méthodes de travail efficaces pour vous apporter un résultat durable. L'agrandissement de votre maison est non seulement un investissement pour votre vie personnelle, mais bien aussi pour la revente de celle-ci donc cela va vous assure un retour sur investissement sur votre agrandissement de maison. Notre équipe va être présente et vous épaulera tout au long de votre projet d'agrandissement de fondation.</p>
+          </div>
+      </div>
+  
+      <div data-paperform-id="lesentrerpisesasraca"></div>
+    </div>
+  </div>
+  <?php include(__DIR__."/../../footing.php")?>
+</body>
+</html>
