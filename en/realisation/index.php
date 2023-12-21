ed0b53016263bceff7b0b03a474aef2b644bbc88
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-12-21 08:57:45 -0500

    realisations

---
+++ en/realisation/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<?php
+$metaDesc = "Here are the results of our team of experts’ work, ranging from concrete steps and concrete balconies to house foundations and even commercial concrete projects and much more! We ensure the highest quality work for each of our projects and the satisfaction of our clients.";
+$englishUrl = "https://coffragesphoenix.com/realisation";
+include_once(__DIR__ . "/../head.php");
+?>
+
+<body>
+  <style>
+    @media only screen and (min-width: 573px) and (max-width: 831px) {
+      h2 {
+        font-size: 40px !important;
+        line-height: 35px !important;
+      }
+
+      .header-1 {
+        font-size: 35px !important;
+      }
+    }
+
+    @media only screen and (min-width: 395px) and (max-width: 572px) {
+      h2 {
+        font-size: 30px !important;
+        line-height: 35px !important;
+      }
+
+      .insta-button a {
+        font-size: 14px !important;
+      }
+    }
+
+    @media only screen and (max-width: 394px) {
+      .insta-button a {
+        font-size: 12px !important;
+      }
+      .red-button {
+        font-size: 12px !important;
+      }
+    }
+
+    .insta-button {
+      text-align: center;
+      margin: 40px 0;
+    }
+
+    h2 {
+      font-family: "Space Grotesk", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif !important;
+      font-size: 45px !important;
+      line-height: 60px !important;
+      font-weight: 400 !important;
+      letter-spacing: 0px !important;
+      color: var(--primaryColor);
+    }
+  </style>
+  <?php include(__DIR__ . "/../header.php") ?>
+
+  <div class="container">
+
+    <div style="display: grid;">
+      <div style="text-align: center;">
+        <h1>Realizations</h1>
+      </div>
+      <div style="text-align: center;margin-left: auto;margin-right: auto;"><img src="https://coffragesphoenix.com/assets/img/about-swig.svg" alt="about" /></div>
+    </div>
+    <div class="container-95pct">
+      <div class="insta-button">
+        <a class="red-button" href="https://www.instagram.com/coffragesphoenix/" target="_blank">Follow our latest realizations on Instagram <i class="fa-brands fa-instagram"></i></a>
+      </div>
+      <div class="alignleft" style="margin: 30px 0;">
+        <h2 class="header-1">Concrete stairs, concrete sidewalks and concrete balconies</h2>
+      </div>
+      <div class="columns-03-333333">
+        <img src="https://coffragesphoenix.com/assets/img/Marche-en-beton_001.jpg" width="800" height="575" alt="Marche-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/Marche-en-beton_002.jpg" width="800" height="575" alt="Marche-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-de-beton_001.jpg" width="800" height="575" alt="Marche-de-beton-laurentides" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-de-beton_002.jpg" width="800" height="575" alt="Marche-de-beton-lanaudiere" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-de-beton_003.jpeg" width="800" height="575" alt="Escalier-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-de-beton_004.jpeg" width="800" height="575" alt="Escalier-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-de-beton_005.jpeg" width="800" height="575" alt="Escalier-de-beton-laurentides" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-trottoir-de-beton_001.jpg" width="800" height="575" alt="Trottoir-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/escalier-trottoir-de-beton_002.jpg" width="800" height="575" alt="Escalier de beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/balcon-de-beton_003.jpg" width="800" height="575" alt="Balcon-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/balcon-de-beton_004.jpg" width="800" height="575" alt="Balcon-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/patio2.jpeg" width="800" height="575" alt="Patio" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/patio.jpeg" width="800" height="575" alt="Patio-en-beton" fetchPriority="high" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/piscine.jpeg" width="800" height="575" alt="Piscine" fetchPriority="high" loading="lazy">
+      </div>
+      <div class="alignleft" style="margin: 25px 0;">
+        <h2>Concrete basement entrance</h2>
+      </div>
+      <div class="columns-02-5050">
+        <img src="https://coffragesphoenix.com/assets/img/descente-de-sous-sol-en-beton_001.jpg" width="800" height="575" alt="Descente-de-sous-sol-en-beton" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/descente-de-sous-sol-en-beton_002.jpg" width="800" height="575" alt="Descente-de-sous-sol-laurentides" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/descente-de-sous-sol-en-beton_003.jpeg" width="800" height="575" alt="Descente-de-sous-sol-rive-nord" loading="lazy">
+      </div>
+      <div class="alignleft" style="margin: 25px 0;">
+        <h2>Foundations for house expansion</h2>
+      </div>
+      <div class="columns-02-5050">
+        <img src="https://coffragesphoenix.com/assets/img/fondation-agrandissement-de-maison_001.jpg" width="800" height="575" alt="Agrandissement-fondation" fetchPriority="low" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/fondation-agrandissement-de-maison_002.jpeg" width="800" height="575" alt="agrandissement-fondation-maison-laurentides" fetchPriority="low" loading="lazy">
+        <img src="https://coffragesphoenix.com/assets/img/fondation-agrandissement-de-maison_003.jpg"="800" height="575" alt="Agrandissement-fondation" fetchPriority="low" loading="lazy">
+      </div>
+    </div>
+
+
+  </div>
+  <?php include(__DIR__ . "/../footing.php") ?>
+</body>
+
+</html>
