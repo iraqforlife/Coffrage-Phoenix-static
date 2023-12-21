c7f65158a841150e8c0d0ade6b62c318b2fa0268
Ahmad <ahmad.al-taher.1@etsmtl.net>
2023-12-21 08:29:18 -0500

    soumission

---
+++ soumission/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<?php 
+    $metaDesc ="Notre équipe d’experts assurera un produit de la meilleure qualité pour vous assurer un produit durable et résistant aux tremblements de terre pour l’agrandissement de votre maison.";
+    $englishUrl = "https://coffragesphoenix.com/en/soumission";
+    include_once(__DIR__."/../head.php");
+?>
+<body >
+  <?php include(__DIR__."/../header.php")?>
+
+  <div class="container">  
+      <div data-paperform-id="lesentrerpisesasraca"></div>
+    </div>
+  </div>
+  <?php include(__DIR__."/../footing.php")?>
+</body>
+</html>
