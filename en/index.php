9fd1c427a67b72b197c98563ad4d8f3cb806c868
Ahmad Al-Taher <ahmadaltahir@gmail.com>
2025-01-23 12:53:03 -0500

    the center text in the home page

--- en/index.php
+++ en/index.php
@@ -11,15 +11,140 @@ include_once(__DIR__ . "/head.php");
 <body>
   <?php include(__DIR__ . "/header.php") ?>
   <style>
-    .service-item,.services-header{text-align:center}.borderBlue,.service-item{border:1px solid #0c4c8e}.slider-header{width:40%}.slider-header h1{margin-bottom:30px;color:#fff!important}@media only screen and (max-width:360px){.slider-header h1{font-size:10px!important;line-height:14px!important;margin:0!important}}@media only screen and (min-width:361px) and (max-width:450px){.slider-header h1{font-size:15px!important;line-height:17px!important;margin:0!important;font-weight:400!important}}@media only screen and (min-width:451px) and (max-width:650px){.slider-header h1{font-size:26px!important;line-height:19px!important;margin-bottom:10px!important;font-weight:400!important}}@media only screen and (min-width:651px) and (max-width:725px){.slider-header h1{font-size:17px!important;line-height:30px!important;margin-bottom:15px!important;font-weight:450!important}}@media only screen and (min-width:726px) and (max-width:900px){.slider-header h1{font-size:24px!important;margin-bottom:15px;line-height:30px!important;font-weight:450!important}}@media only screen and (min-width:901px) and (max-width:1000px){.slider-header h1{font-size:35px!important;line-height:40px!important;font-weight:450!important;margin-bottom:20px}}@media only screen and (min-width:1001px) and (max-width:1100px){.slider-header h1{font-size:36px!important;line-height:45px!important;font-weight:450!important}}@media only screen and (min-width:1101px) and (max-width:1200px){.slider-header h1{font-size:44px!important;line-height:45px!important}}@media only screen and (min-width:1201px) and (max-width:1300px){.slider-header h1{font-size:42px!important;line-height:53px!important}}@media only screen and (max-width:550px){.slider-header .socials{display:none}}.service-item{margin:10px;max-width:650px;min-width:340px;box-shadow:0 4px 5px 0 rgba(12,76,142,.38)}.service-item p{margin:15px;font-size:20px;line-height:28px;font-weight:500;letter-spacing:0;color:#0c4c8e}.servicesSlider{display:grid;grid-template-columns:5% 90% 5%}.hide{display:none}.serviceItems{display:flex;flex-wrap:nowrap;white-space:nowrap}.verticleCenter{position:absolute;display:flex;align-items:center;justify-content:center;align-items:center}.carouselButtons{outline:0;background:0 0;border:none;cursor:pointer}#carousel {display:flex;align-items:center;overflow-x:auto;scroll-behavior:smooth}#left,#right{z-index:10}.sliderButton{background-color:#ced4da;width:3rem;height:.6rem;transition:background-color .2s,color .2s,box-shadow .2s;border-radius:0;border-width:0}
+    
+    .slider-header {
+      text-align: center;
+      top: 20%;
+    }
+
+    .slider-header h1 {
+      margin-bottom: 30px;
+      color: #fff !important
+    }
+
+    @media only screen and (max-width:360px) {
+      .slider-header h1 {
+        font-size: 10px !important;
+        line-height: 14px !important;
+        margin: 0 !important;
+      }
+    }
+    @media only screen and (max-width:550px) {
+      .slider-header .socials {
+        font-size: 10px !important;
+        margin-top: 10px !important;
+      }
+      .button-border {
+        font-size: 18px !important;
+        border: 2px solid #FFF !important;
+        padding: 8px 13px !important;
+      }
+      .services-header h2 {
+        display: none !important;
+      }
+    }
+
+    @media only screen and (min-width:361px) and (max-width:450px) {
+      .slider-header h1 {
+        font-size: 23px !important;
+        line-height: 22px !important;
+        margin: 0 !important;
+        font-weight: 400 !important;
+      }
+    }
+
+    @media only screen and (min-width:451px) and (max-width:650px) {
+      .slider-header h1 {
+        font-size: 26px !important;
+        line-height: 19px !important;
+        margin-bottom: 10px !important;
+        font-weight: 400 !important
+      }
+    }
+
+    @media only screen and (min-width:651px) and (max-width:725px) {
+      .slider-header h1 {
+        font-size: 17px !important;
+        line-height: 30px !important;
+        margin-bottom: 15px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:726px) and (max-width:900px) {
+      .slider-header h1 {
+        font-size: 24px !important;
+        margin-bottom: 15px;
+        line-height: 30px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:901px) and (max-width:1000px) {
+      .slider-header h1 {
+        font-size: 35px !important;
+        line-height: 40px !important;
+        font-weight: 450 !important;
+        margin-bottom: 20px
+      }
+    }
+
+    @media only screen and (min-width:1001px) and (max-width:1100px) {
+      .slider-header h1 {
+        font-size: 36px !important;
+        line-height: 45px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:1101px) and (max-width:1300px) {
+      .slider-header h1 {
+        font-size: 42px !important;
+        line-height: 53px !important
+      }
+    }
+
+
+    .hide {
+      display: none
+    }
+
+    .verticleCenter {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      align-items: center
+    }
+
+    #carousel {
+      display: flex;
+      align-items: center;
+      overflow-x: auto;
+      scroll-behavior: smooth
+    }
+
+    #left,
+    #right {
+      z-index: 10
+    }
+
+    .sliderButton {
+      background-color: #ced4da;
+      width: 3rem;
+      height: .6rem;
+      transition: background-color .2s, color .2s, box-shadow .2s;
+      border-radius: 0;
+      border-width: 0
+    }
   </style>
   <div class="container">
     <div style="position: relative;z-index: 2;">
-      <div class="absolute container-91pct top-050px left-003pct slider-header">
+      <div class="absolute container-91pct left-003pct slider-header">
         <h1 style="color:white;">Concrete balcony, side walk, slab and formwork specialist in Montreal, Laval, Terrebonne and the North Shore of Montreal.</h1>
         <div class="spacer-001"></div>
         <a class="button-border" href="#quote">Quote&nbsp; &#10095;</a>
-        <div class="socials fonthover-06" style="margin-top: 5px;">
+        <div class="socials fonthover-06" style="margin-top: 15px;">
           <a class="reseau-button" href="https://www.facebook.com/coffragesphoenix/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
           <a class="reseau-button" href="https://www.instagram.com/coffragesphoenix/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
           <a class="reseau-button" href="https://twitter.com/CoffragePhoenix/" title="Twitter" target="_blank"><i class="fa-brands fa-twitter"></i></a>
@@ -27,7 +152,7 @@ include_once(__DIR__ . "/head.php");
         </div>
       </div>
       <div style="overflow: hidden;">
-        <img src="/assets/img/coffrage-de-beton-sainte-anne-des-plaines.webp" style="min-height:100px;max-height:850px;object-fit:cover;width: 200%;position:relative;margin:0;left:0" alt="Coffrage-de-beton-rive-nord">
+        <img src="/assets/img/coffrage-de-beton-sainte-anne-des-plaines.webp" style="min-height:250px;max-height:530px;object-fit:cover;width: 200%;position:relative;margin:0;left:0" alt="Coffrage-de-beton-rive-nord">
       </div>
     </div>
 
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html  lang="fr">
+<!DOCTYPE html lang="fr">
 <html lang="fr-CA">
 <?php
 $metaDesc = "Spécialiste en balcon, trottoir et dalle de béton et coffrages à Montréal, Laval, Terrebonne et la Rive-Nord";
@@ -11,15 +11,139 @@ include_once(__DIR__ . "/head.php");
 <body>
   <?php include(__DIR__ . "/header.php") ?>
   <style>
-    .slider-header{width:40%}.slider-header h1{margin-bottom:30px;color:#fff!important}@media only screen and (max-width:360px){.slider-header h1{font-size:10px!important;line-height:14px!important;margin:0!important}}@media only screen and (min-width:361px) and (max-width:450px){.slider-header h1{font-size:15px!important;line-height:17px!important;margin:0!important;font-weight:400!important}}@media only screen and (min-width:451px) and (max-width:650px){.slider-header h1{font-size:26px!important;line-height:19px!important;margin-bottom:10px!important;font-weight:400!important}}@media only screen and (min-width:651px) and (max-width:725px){.slider-header h1{font-size:17px!important;line-height:30px!important;margin-bottom:15px!important;font-weight:450!important}}@media only screen and (min-width:726px) and (max-width:900px){.slider-header h1{font-size:24px!important;margin-bottom:15px;line-height:30px!important;font-weight:450!important}}@media only screen and (min-width:901px) and (max-width:1000px){.slider-header h1{font-size:35px!important;line-height:40px!important;font-weight:450!important;margin-bottom:20px}}@media only screen and (min-width:1001px) and (max-width:1100px){.slider-header h1{font-size:36px!important;line-height:45px!important;font-weight:450!important}}@media only screen and (min-width:1101px) and (max-width:1200px){.slider-header h1{font-size:44px!important;line-height:45px!important}}@media only screen and (min-width:1201px) and (max-width:1300px){.slider-header h1{font-size:42px!important;line-height:53px!important}}@media only screen and (max-width:550px){.slider-header .socials{display:none}}.hide{display:none}.verticleCenter{position:absolute;display:flex;align-items:center;justify-content:center;align-items:center}#carousel{display:flex;align-items:center;overflow-x:auto;scroll-behavior:smooth}#left,#right{z-index:10}.sliderButton{background-color:#ced4da;width:3rem;height:.6rem;transition:background-color .2s,color .2s,box-shadow .2s;border-radius:0;border-width:0}
+    .slider-header {
+      text-align: center;
+      top: 20%;
+    }
+
+    .slider-header h1 {
+      margin-bottom: 30px;
+      color: #fff !important
+    }
+
+    @media only screen and (max-width:360px) {
+      .slider-header h1 {
+        font-size: 10px !important;
+        line-height: 14px !important;
+        margin: 0 !important;
+      }
+    }
+    @media only screen and (max-width:550px) {
+      .slider-header .socials {
+        font-size: 10px !important;
+        margin-top: 10px !important;
+      }
+      .button-border {
+        font-size: 18px !important;
+        border: 2px solid #FFF !important;
+        padding: 8px 13px !important;
+      }
+      .services-header h2 {
+        display: none !important;
+      }
+    }
+
+    @media only screen and (min-width:361px) and (max-width:450px) {
+      .slider-header h1 {
+        font-size: 23px !important;
+        line-height: 22px !important;
+        margin: 0 !important;
+        font-weight: 400 !important;
+      }
+    }
+
+    @media only screen and (min-width:451px) and (max-width:650px) {
+      .slider-header h1 {
+        font-size: 26px !important;
+        line-height: 19px !important;
+        margin-bottom: 10px !important;
+        font-weight: 400 !important
+      }
+    }
+
+    @media only screen and (min-width:651px) and (max-width:725px) {
+      .slider-header h1 {
+        font-size: 17px !important;
+        line-height: 30px !important;
+        margin-bottom: 15px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:726px) and (max-width:900px) {
+      .slider-header h1 {
+        font-size: 24px !important;
+        margin-bottom: 15px;
+        line-height: 30px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:901px) and (max-width:1000px) {
+      .slider-header h1 {
+        font-size: 35px !important;
+        line-height: 40px !important;
+        font-weight: 450 !important;
+        margin-bottom: 20px
+      }
+    }
+
+    @media only screen and (min-width:1001px) and (max-width:1100px) {
+      .slider-header h1 {
+        font-size: 36px !important;
+        line-height: 45px !important;
+        font-weight: 450 !important
+      }
+    }
+
+    @media only screen and (min-width:1101px) and (max-width:1300px) {
+      .slider-header h1 {
+        font-size: 42px !important;
+        line-height: 53px !important
+      }
+    }
+
+
+    .hide {
+      display: none
+    }
+
+    .verticleCenter {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      align-items: center
+    }
+
+    #carousel {
+      display: flex;
+      align-items: center;
+      overflow-x: auto;
+      scroll-behavior: smooth
+    }
+
+    #left,
+    #right {
+      z-index: 10
+    }
+
+    .sliderButton {
+      background-color: #ced4da;
+      width: 3rem;
+      height: .6rem;
+      transition: background-color .2s, color .2s, box-shadow .2s;
+      border-radius: 0;
+      border-width: 0
+    }
   </style>
   <div class="container">
     <div style="position: relative;z-index: 2;">
-      <div class="absolute container-91pct top-050px left-003pct slider-header">
+      <div class="absolute container-91pct left-003pct slider-header">
         <h1 class="home-header" style="color:white;">Spécialiste en balcon, trottoir et dalle de béton et coffrages à Montréal, Laval, Terrebonne et la Rive-Nord</h1>
         <div class="spacer-001"></div>
         <a class="button-border" href="#soumission">Soumission&nbsp; &#10095;</a>
-        <div class="socials fonthover-06" style="margin-top: 5px;">
+        <div class="socials fonthover-06" style="margin-top: 15px;">
           <a class="reseau-button" href="https://www.facebook.com/coffragesphoenix/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
           <a class="reseau-button" href="https://www.instagram.com/coffragesphoenix/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
           <a class="reseau-button" href="https://twitter.com/CoffragePhoenix/" title="Twitter" target="_blank"><i class="fa-brands fa-twitter"></i></a>
@@ -27,7 +151,7 @@ include_once(__DIR__ . "/head.php");
         </div>
       </div>
       <div style="overflow: hidden;">
-        <img src="/assets/img/coffrage-de-beton-sainte-anne-des-plaines.webp" style="min-height:100px;max-height:850px;object-fit:cover;width: 200%;position:relative;margin:0;left:0" alt="Coffrage-de-beton-rive-nord">
+        <img src="/assets/img/coffrage-de-beton-sainte-anne-des-plaines.webp" style="min-height:250px;max-height:530px;object-fit:cover;width: 200%;position:relative;margin:0;left:0" alt="Coffrage-de-beton-rive-nord">
       </div>
     </div>
 
@@ -106,13 +230,13 @@ include_once(__DIR__ . "/head.php");
       </div>
 
       <?php include(__DIR__ . "/values.php") ?>
-      
+
       <div style="text-align: center;margin-bottom: 35px;">
         <a href="/entreprise/" class="blue-button">En savoir plus sur l'entreprise</a>
       </div>
-      
+
       <div id="soumission" data-paperform-id="lesentrerpisesasraca"></div>
-      
+
       <?php include(__DIR__ . "/reviews.php") ?>
     </div>
 
